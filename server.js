7063515988314cb52ce1a3783b3593a162f1fbce
liberaltech <liberaltech@users.noreply.github.com>
2016-12-28 21:23:05 -0500

    first deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //  OpenShift sample Node application
 var express = require('express'),
-    fs      = require('fs'),
+		fs      = require('fs'),
+		path 		= require('path');
     app     = express(),
     eps     = require('ejs'),
     morgan  = require('morgan');
